9637fc8082b8a9e2a75884e99733745a28117ece
Jakub Ružička <jakub.ruzicka@nic.cz>
2024-05-14 13:12:48 +0200

    Draft: CI: build Arch images periodically
    Arch Linux is broken every other week so rebuild the image periodically.
    
    BTW I'm not using Arch.

--- ci/images/update_all.sh
+++ ci/images/update_all.sh
@@ -3,10 +3,17 @@
 # fail early
 set -e
 
+IMAGES=$@
+
 # run build scripts in the correct order (be careful, containers from some directories depend on containers from other directories)
 for dir in test systemd full; do
 	echo "Building $dir"
 	cd $dir
-	./update.sh $(ls -p | grep / | sed 's#/##')
+	if [ -n "$IMAGES" ]; then
+		images=$IMAGES
+	else
+		images=$(ls -p | grep / | sed 's#/##')
+	fi
+	./update.sh $images
 	cd ..
 done
